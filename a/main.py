7341c9fee9e26592c388386f1758d2a3ea1a0192
LukeStevens0n <lstevenson63@gmail.com>
2019-12-18 16:35:32 +0000

    Added train all script

--- a/main.py
+++ a/main.py
@@ -5,39 +5,33 @@ import torch.optim as optim
 from torch.distributions import Normal
 import os
 import torch
-import torch.multiprocessing as _mp
 from src.environment import create_env
 from actorcritic import Actor_Critic
 from src.convolutional_ae import CAE
 from src.SharedAdam import SharedAdam
 from train import train
 
-torch.manual_seed(123)
+def run_a ():
+    torch.manual_seed(123)
+    button = 'A'
+    env, num_states, num_actions = create_env(1,1,button,5)
 
-mp = _mp.get_context('spawn')
-button = 'A'
-env, num_states, num_actions = create_env(1,1,button,5)
+    print("num states: {}".format(num_states))
+    print("num actions: {}".format(num_actions))
+    print("env: {}".format(env))
 
-print("num states: {}".format(num_states))
-print("num actions: {}".format(num_actions))
-print("env: {}".format(env))
+    CAE_shared_model = CAE()
+    A3C_shared_model = Actor_Critic(num_states,num_actions)
 
-CAE_shared_model = CAE()
-A3C_shared_model = Actor_Critic(num_states,num_actions)
-
-CAE_shared_model.share_memory()
-A3C_shared_model.share_memory()
-#print("A3C - shared")
-#print(A3C_shared_model)
-#print("num states: {} , num actions: {}".format(num_states,num_actions))
+    CAE_shared_model.share_memory()
+    A3C_shared_model.share_memory()
+    #print("A3C - shared")
+    #print(A3C_shared_model)
+    #print("num states: {} , num actions: {}".format(num_states,num_actions))
 
 
-optimiser_cae = CAE_shared_model.createLossAndOptimiser(CAE_shared_model,0.001)
-optimiser_a3c = SharedAdam(A3C_shared_model.parameters(),lr =0.001)
+    optimiser_cae = CAE_shared_model.createLossAndOptimiser(CAE_shared_model,0.001)
+    optimiser_a3c = SharedAdam(A3C_shared_model.parameters(),lr =0.001)
 
-workers = []
-
-# for rank in range(1):
-#     worker = mp.Process(target = train, args = (rank, optimiser_a3c,A3C_shared_model,CAE_shared_model,optimiser_cae,True))
-
-train(1, optimiser_a3c,A3C_shared_model,CAE_shared_model,optimiser_cae,True)
+    
+    train(1, optimiser_a3c,A3C_shared_model,CAE_shared_model,optimiser_cae,True)
---
+++ src/train_all.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("C:\\Users\\UKGC-PC\\Documents\\Level 4 Project")
+from a.main import run_a
+from b.main import run_b
+from up.main import run_up
+from down.main import run_down
+from left.main import run_left
+from right.main import run_right
+from run_agent import run_test
+# run all train scripts then execute
+
+run_a()
+run_b()
+run_down()
+run_left()
+run_up()
+run_right()
+run_test()
