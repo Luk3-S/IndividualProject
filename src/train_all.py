f4b5fd427c5a08edc97e21f3f82b5eddaa693e1e
Luke Stevenson <2242306s@student.gla.ac.uk>
2020-01-23 16:30:30 +0000

    Implemented multithreading in train all script

--- src/train_all.py
+++ src/train_all.py
@@ -1,5 +1,8 @@
 import sys
-sys.path.append("C:\\Users\\UKGC-PC\\Documents\\Level 4 Project")
+import multiprocessing
+from multiprocessing import Pool
+#sys.path.append("C:\\Users\\UKGC-PC\\Documents\\Level 4 Project")
+sys.path.append("C:\\Users\\Luke\\Documents\\metal-mario")
 from a.main import run_a
 from b.main import run_b
 from up.main import run_up
@@ -8,10 +11,17 @@ from left.main import run_left
 from right.main import run_right
 from run_agent import run_test
 # run all train scripts then execute
+from pathlib import Path
 
-run_a()
-run_b()
-run_down()
-run_left()
-run_up()
-run_right()
+
+buttons_to_train = [run_a(),
+run_b(),
+run_down(),
+run_left(),
+run_up(),
+run_right(),
+]
+
+if __name__ ==  '__main__':
+    for function in buttons_to_train:
+        Pool(2).starmap(function,[() for _ in range(5)])
